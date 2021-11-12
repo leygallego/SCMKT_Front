4e063b7a64eb371ec70cd95125bad4b43c6574e1
leygallego <leyder.gallego@gmail.com>
2021-11-11 19:27:14 -0500

    Creación componente detalle Contrato

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavBar from './componentes/NavBar';
 import Home from './componentes/Home';
 import Contratos from './componentes/Contratos';
 import AboutUs from './componentes/AboutUs';
+import DetalleContrato from './componentes/DetalleContrato';
 
 
 
@@ -17,7 +18,9 @@ function App() {
         <Switch>
         <Route exact path="/home" component={Home}></Route>   
         <Route exact path="/contratos" component={Contratos}></Route>  
-        <Route  exact path="/aboutus" component={AboutUs}></Route>   
+        <Route  exact path="/aboutus" component={AboutUs}></Route>  
+        <Route  exact path="/detalle" component={DetalleContrato}></Route>   
+ 
         </Switch>
      
 
