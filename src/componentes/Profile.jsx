f8d4d075781e72641d7b7ec03152ade40a7c612f
leygallego <leyder.gallego@gmail.com>
2021-11-21 22:31:15 -0500

    Avances en integración formulario edición en componente pefil

--- src/componentes/Profile.jsx
+++ src/componentes/Profile.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Profile.css';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import Button from '@mui/material/Button';
 import CreateIcon from '@mui/icons-material/Create';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { postSingUp } from '../actions';
+
 
 
 
@@ -12,8 +14,88 @@ function Profile() {
 
     const usuarios = useSelector(state => state.users)
     const contratos = useSelector(state => state.contracts)
+    const dispatch = useDispatch();
+
+    
+
     console.log("Contratos", contratos);
     console.log("Usuarios",usuarios);
+
+    const Countries = ["", "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia",
+    "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize",
+    "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso",
+    "Burundi", "Côte d'Ivoire", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile",
+    "China", "Colombia", "Comoros", "Congo (Congo-Brazzaville)", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czechia (Czech Republic)",
+    "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt",
+    "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini (fmr. 'Swaziland')", "Ethiopia", "Fiji", "Finland",
+    "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana",
+    "Haiti", "Holy See", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
+    "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia",
+    "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",
+    "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco",
+    "Mozambique", "Myanmar (formerly Burma)", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua",
+    "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea",
+    "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis",
+    "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal",
+    "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea",
+    "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste",
+    "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates",
+    "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"];
+
+    const [edicionPerfil, setEdicionPerfil] = useState(true)
+    const [bool, setBool] = useState(true);
+    const [registro, setRegistro] = useState({
+        username: "",
+        country: "",
+        email: "",
+        wallet: "",
+        password: ""
+    });
+
+    const handleOnChange = (e) => {
+        setRegistro({
+            ...registro,
+            [e.target.name]: e.target.value
+        })
+        console.log(registro)
+    }
+
+
+    const handleEdition = ()=>{
+        setEdicionPerfil(false)
+    }
+
+    const handleOnClick = () => {
+        setBool(false)
+    }
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        console.log(registro)
+        if (
+            registro.username === "" || registro.country === ""
+            || registro.email === "" || registro.wallet === ""
+            || registro.password === ""
+
+        ) {
+            console.log('Formulario incompleto...')
+        } else {
+            dispatch(postSingUp(registro));
+            console.log("el registro", registro);
+            
+            setRegistro({
+                username: "",
+                country: "",
+                email: "",
+                wallet: "",
+                password: ""
+            });
+            alert("Enviado correctamente")
+
+            
+            // window.location.href = baseUrl
+        }
+    }
 
     return (
         <>
@@ -34,26 +116,7 @@ function Profile() {
                             </div>
                         )
                     })}
-                    {/* <div className="info-contrato">
-                        <h6>Nombre del contrato</h6>
-                        <h6>0.056489398</h6>
-                        <h6>view details</h6>
-                    </div> */}
-                    {/* <div className="info-contrato">
-                        <h6>Nombre del contrato</h6>
-                        <h6>0.056489398</h6>
-                        <h6>view details</h6>
-                    </div>
-                    <div className="info-contrato">
-                        <h6>Nombre del contrato</h6>
-                        <h6>0.056489398</h6>
-                        <h6>view details</h6>
-                    </div>
-                    <div className="info-contrato">
-                        <h6>Nombre del contrato</h6>
-                        <h6>0.056489398</h6>
-                        <h6>view details</h6>
-                    </div> */}
+                  
                 </div>
                 <div className="contratos-borradores">
                 <h5>Borradores</h5>
@@ -98,6 +161,8 @@ function Profile() {
 
             <div className="area-perfil">
             <img src="/images/silueta.png" alt="imagen de silueta" />
+
+
                 
             <Button
                             className="busca-wallet"
@@ -105,15 +170,65 @@ function Profile() {
                             startIcon={<AccountBalanceWalletIcon />}
                         >Wallet Address</Button>
                     <div className="datos-personales" >
+
+
                     <Button
                             className="busca-datos"
                             variant="contained"
                             startIcon={<CreateIcon />}
+                            onClick={handleEdition}
                         >Datos Personales</Button> 
+                        {
+                            edicionPerfil ? <div>
                         <h6>Fulano de tal</h6> 
                         <h6>Wakanda</h6>
                         <h6>fulano@gmail.com</h6>
                         <h6>25/01/2001</h6>
+                            </div> : 
+                            <form onSubmit={e => { handleOnSubmit(e) }}>
+                            {bool ? <div className="registro1">
+                                <div className="labelInput">
+                                    <div className="labelForm">User name</div>
+                                    <div className="inputForm"><input className="inputFormComponent" type="text" name="username" onChange={e => { handleOnChange(e) }} defaultValue={registro.name} /></div>
+                                </div>
+                                <div className="labelInput">
+                                    <div className="labelForm">Country</div>
+                                    <div>
+                                        <select className="inputFormComponent" name="country" onChange={e => { handleOnChange(e) }} defaultValue={registro.country} >
+                                            {
+                                                Countries.map((element, index) => {
+                                                    return (
+                                                        <option key={index}>{element}</option>
+                                                    )
+                                                })
+                                            }
+                                        </select></div>
+                                </div>
+                                <div className="labelInput">
+                                    <div className="labelForm">Email</div>
+                                    <div className="inputForm"><input className="inputFormComponent" type="email" name="email" onChange={e => { handleOnChange(e) }} defaultValue={registro.email} /></div>
+                                </div>
+                                <div className="buttonFormComponent"><input className="buttonComponent" type="button" value="Next" onClick={handleOnClick} /></div>
+                            </div>
+                                : <div className="registro2">
+                                    <div className="labelInput">
+                                        <div className="labelForm">Wallet address</div>
+                                        <div className="inputForm"><input className="inputFormComponent" type="text" name="wallet" onChange={e => { handleOnChange(e) }} defaultValue={registro.wallet} /></div>
+                                    </div>
+                                    <div className="labelInput">
+                                        <div className="labelForm">Password</div>
+                                        <div className="inputForm"><input className="inputFormComponent" type="password" name="password" onChange={e => { handleOnChange(e) }} defaultValue={registro.password} /></div>
+    
+                                    </div>
+                                    <div className="labelInput">
+                                        <div className="labelForm">Confirm your password</div>
+                                        <div className="inputForm"><input className="inputFormComponent" type="password" /></div>
+                                    </div>
+                                    <div className="buttonFormComponent"><input className="buttonComponent" type="submit" value="Send" /></div>
+                                </div>}
+                        </form>
+                        }
+                        
                         {/* {usuarios.map((usuario, index) =>{
                            return (<div key={index}>
                                <h6>{usuario.name}</h6>
