db2f8854757a94e40d530608fb100a41bc4da184
Eber Hernández <ebershr@gmail.com>
2021-11-24 16:09:23 -0500

    Revisión de Filtros y Upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-// require('dotenv').config();
-
-console.log(process.env.REACT_APP_APIKEY);
+require('dotenv').config();
 
 const firebaseConfig = {
     apiKey: "AIzaSyDS2_dlr3yeMOYlvstxLelsm9jtUcKf1H8",
