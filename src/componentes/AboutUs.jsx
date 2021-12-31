87d376de527e6c3311faf2c2446221e350268627
leygallego <leyder.gallego@gmail.com>
2021-12-31 10:38:01 -0500

    Avances  About us y correccion paginacion

--- src/componentes/AboutUs.jsx
+++ src/componentes/AboutUs.jsx
@@ -17,14 +17,9 @@ function AboutUs() {
         <>
             <div className="about-container">
                 <div className="decorative-about">
-                    <img src="./images/decorative.png" alt="imagen decorativa"/>
+                    <img src="./images/decorative.jpg" alt="imagen decorativa"/>
                 </div>
-                <div className="leyder">
-                <img src="./images/leyder.png" alt="imagen decorativa"/>
-                </div>
-                <div className="benito">
-                <img src="./images/benito.jpeg" alt="imagen decorativa"/>
-                </div>
+                
                 <div className="texto-about">
                     <h1>Somos (scmkt)</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat deleniti libero suscipit adipisci, corporis sapiente ab voluptas et! Neque rerum officiis quisquam obcaecati. Corporis eaque vel deserunt velit, mollitia non? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum sunt iusto nobis facilis molestias sint repellat itaque dolore id laudantium aliquam corporis</p>
