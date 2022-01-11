4ce290c39465d0b743ae9e9214aa570524e7aaa6
Nitro <62272124+zzzNitro@users.noreply.github.com>
2022-01-11 16:13:17 -0300

    Delete app.config.js

--- src/config/app.config.js
+++
@@ -1,8 +0,0 @@
-// export const NODE_ENV = 'production' // Deploy
-export const NODE_ENV = 'production1' // Localhost front y Deploy Back
-
-export const urlProduction1 = 'https://scmkt.herokuapp.com'
-export const urlProduction = 'https://scmkt-4fe6b.web.app'
-export const urlDevelop = 'http://localhost'
-export const port1 = '3001'
-export const port2 = '3000'
